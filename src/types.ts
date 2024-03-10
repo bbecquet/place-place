a159e05b7f334cfd413603b96d45878c984db3c9
bbecquet <benjamin.becquet@gmail.com>
2024-03-10 21:16:27 +0100

    Add id to points

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,9 @@
 import { LatLng } from 'leaflet'
 
 export type GamePoint = {
+  id: string
   name: string
   picto: string
   position: [number, number]
   userPosition: LatLng
-}
-
-export type Game = {
-  place: string
-  points: GamePoint[]
 }
