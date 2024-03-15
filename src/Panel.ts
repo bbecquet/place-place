47ea3ecc75d4b15866db0aa90324c016ece80847
bbecquet <benjamin.becquet@gmail.com>
2024-03-15 14:30:49 +0100

    WIP proper dom manipulation

--- src/Panel.ts
+++ src/Panel.ts
@@ -3,6 +3,37 @@ import { formatDistance } from './utils'
 import { startIcon, restartIcon, checkIcon, fastForwardIcon } from './icons'
 import { pointPanelItem } from './point'
 
+type DomContent = string | Node | Array<string | Node>
+
+const ce = (
+  tagName: string,
+  attributes: Record<string, string>,
+  content?: DomContent
+): HTMLElement => {
+  console.log('plup')
+  const elt = document.createElement(tagName)
+  Object.entries(attributes).forEach(([k, v]) => {
+    elt.setAttribute(k, v)
+  })
+  if (content) {
+    ec(elt, content)
+  }
+  return elt
+}
+
+const ec = (elt: HTMLElement, content: DomContent) => {
+  // TODO: replaceChildren
+  if (Array.isArray(content)) {
+    content.forEach(n => {
+      ec(elt, n)
+    })
+  } else if (typeof content === 'string') {
+    elt.innerHTML = content
+  } else {
+    elt.appendChild(content)
+  }
+}
+
 class Panel {
   panel: HTMLElement
   onStart: () => void
@@ -31,27 +62,24 @@ class Panel {
     this.onJumpToResult = onJumpToResult
   }
 
-  _setContent = (content: string | Node) => {
-    if (typeof content === 'string') {
-      this.panel.innerHTML = content
-    } else {
-      this.panel.innerHTML = ''
-      this.panel.appendChild(content)
-    }
+  _setContent = (content: DomContent) => {
+    ec(this.panel, content)
   }
 
   setMessage(status: 'lastPoint' | 'scoring') {
     if (status === 'lastPoint') {
-      this._setContent(`
-        <p>Vous pouvez encore changer la position des points avant de les vérifier.</p>
-        <button id="finishButton">${checkIcon} Vérifier les positions</button>`)
+      this._setContent([
+        ce('p', {}, 'Vous pouvez encore changer la position des points avant de les vérifier.'),
+        ce('button', { id: 'finishButton' }, `${checkIcon} Vérifier les positions`),
+      ])
       document.getElementById('finishButton')?.addEventListener('click', this.onEnd)
     } else if (status === 'scoring') {
-      this._setContent(`
-        <div class="detailedResults">
-          <ul id="pointScores"></ul>
-          <button id="speedupScoring">${fastForwardIcon} Score final</button>
-        </div>`)
+      this._setContent([
+        ce('div', { class: 'detailedResults' }, [
+          ce('ul', { id: 'pointScores' }),
+          ce('button', { id: 'speedupScoring' }, `${fastForwardIcon} Score final`),
+        ]),
+      ])
       document.getElementById('speedupScoring')?.addEventListener('click', evt => {
         this.onJumpToResult()
         ;(evt.target as HTMLElement).remove()
@@ -61,25 +89,33 @@ class Panel {
 
   setNewGame(placeName: string, points: GamePoint[]) {
     this.panel.className = 'new'
-    this._setContent(`
-        <p>Ceci est une carte de <b>${placeName}</b>.</p>
-        <p>Les points suivants sont déjà placés&nbsp:</p>
-        <ul>${points.map(pt => `<li>${pointPanelItem(pt)}</li>`).join('')}</ul>
-        <p>Saurez-vous placer les autres ?</p>
-        <button id="startButton">${startIcon} Démarrer</button>`)
+    this._setContent([
+      ce('p', {}, `Ceci est une carte de <b>${placeName}</b>.`),
+      ce('p', {}, `Les points suivants sont déjà placés&nbsp:`),
+      ce(
+        'ul',
+        {},
+        points.map(pt => ce('li', {}, pointPanelItem(pt)))
+      ),
+      ce('p', {}, `Saurez-vous placer les autres ?`),
+      ce('button', { id: 'startButton' }, `${startIcon} Démarrer</button>`),
+    ])
     document.getElementById('startButton')?.addEventListener('click', this.onStart)
   }
 
   setPoint(point: GamePoint, isFirst?: boolean) {
     this.panel.className = 'placing'
-    this._setContent(`
-    <p>Cliquez sur la carte pour placer</p>
-    ${pointPanelItem(point, 'currentPoint')}
-    ${
-      !isFirst
-        ? '<p class="small">Vous pouvez aussi déplacer les points précédents.</p>'
-        : '<p class="small">Vous pouvez déplacer et zoomer/dézoomer la carte.</p>'
-    }`)
+    this._setContent([
+      ce('p', {}, 'Cliquez sur la carte pour placer'),
+      pointPanelItem(point, 'currentPoint'),
+      ce(
+        'p',
+        { class: 'small' },
+        !isFirst
+          ? 'Vous pouvez aussi déplacer les points précédents.'
+          : 'Vous pouvez déplacer et zoomer/dézoomer la carte.'
+      ),
+    ])
   }
 
   setScore(score: number) {
@@ -99,15 +135,15 @@ class Panel {
   updateScoringDistance(point: GamePoint, distance: number, color: string) {
     let dist = document.querySelector('#pointScore_' + point.id + ' .dist')
     if (!dist) {
+      dist = ce('div', { class: 'dist' })
       const list = document.getElementById('pointScores') as HTMLElement
-      const li = document.createElement('li')
-      li.id = 'pointScore_' + point.id
-      li.className = 'pointScore'
-      dist = document.createElement('div')
-      dist.className = 'dist'
-      li.innerHTML = pointPanelItem(point)
-      li.appendChild(dist)
-      list.appendChild(li)
+      ec(
+        list,
+        ce('li', { id: 'pointScore_' + point.id, class: 'pointScore' }, [
+          pointPanelItem(point),
+          dist,
+        ])
+      )
     }
     ;(dist.parentElement as HTMLElement).style.cssText = '--color:' + color
     dist.innerHTML = formatDistance(distance, true)
